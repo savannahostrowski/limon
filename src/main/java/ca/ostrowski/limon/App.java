cbbf92a277c162ef5176fba16684c61383b0459d
Savannah Ostrowski <savannahostrowski@gmail.com>
2016-09-20 21:10:13 -0700

    refactor

--- src/main/java/ca/ostrowski/limon/App.java
+++ src/main/java/ca/ostrowski/limon/App.java
@@ -4,36 +4,36 @@ import org.apache.commons.io.FilenameUtils;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 
 import static spark.Spark.*;
 
 public class App {
-    public static void main( String[] args ) throws IOException {
+    public static void main(String[] args) throws IOException {
         App.directorySearch("/home/savannah/Documents/code/limon/");
 
     }
+
     private static Set<String> movieExtensions = new HashSet<>(Arrays.asList("mp4", "avi", "mkv", "webm", "mov", "wmv",
             "m4p", "m4v", "mpg", "mp2", "mpeg", "mpe", "mpv", "m2v"));
 
-    private static List<String> movies = new ArrayList<>();
-
     public static String directorySearch(String rootDir) throws IOException {
+        List<String> movies = new ArrayList<>();
         Files.walk(Paths.get(rootDir)).forEach(filePath -> {
-            if (Files.isRegularFile(filePath)) {
-                String ext = FilenameUtils.getExtension(filePath.toString());
-                for (String extensionInstance: movieExtensions) {
-                    if (ext.equals(extensionInstance)) {
-                        movies.add(FilenameUtils.getBaseName(filePath.toString()));
-                    }
+            if (!Files.isRegularFile(filePath)) {
+                return;
+            }
+            String ext = FilenameUtils.getExtension(filePath.toString());
+            for (String extensionInstance: movieExtensions) {
+                if (ext.equals(extensionInstance)) {
+                    movies.add(FilenameUtils.getBaseName(filePath.toString()));
                 }
             }
         });
-        if (movies.size() != 0) {
-            for (String movie: movies) {
-                System.out.println(movie);
-            }
+        for (String movie: movies) {
+            System.out.println(movie);
         }
         return movies.toString();
     }
